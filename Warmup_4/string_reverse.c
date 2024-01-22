583fa79b3013cfa1f0934b5387c53165c7089d39
Nick Calhoun <nickfilms32@gmail.com>
2024-01-22 02:59:30 +0000

    added fourth warmup

---
+++ Warmup_4/string_reverse.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+   char str[] = "Hello";
+   int length = 0;
+
+   while (str[length] != '\0') {
+   length++;
+   }
+
+   for (int i = 0, j = length - 1; i < j; i++, j--) {
+   char temp = str[i];
+   str[i] = str[j];
+   str[j] = temp;
+   }
+
+   printf("%s\n", str);
+
+   return 0;
+}
